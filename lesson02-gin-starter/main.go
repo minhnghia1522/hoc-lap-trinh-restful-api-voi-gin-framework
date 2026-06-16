a49874ab60269f2db90a47b6cd6a1b1fa5d00488
minhnghia1522 <lmnghia22@gmail.com>
2026-06-16 14:24:07 +0700

    Add user and product endpoints to main application

--- lesson02-gin-starter/main.go
+++ lesson02-gin-starter/main.go
@@ -5,9 +5,42 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"data": []string{"Alice", "Bob", "Charlie"},
+		})
+	})
+
+	r.GET("/user/:user_id", func(ctx *gin.Context) {
+		userId := ctx.Param("user_id")
+		ctx.JSON(200, gin.H{
+			"data": "User ID: " + userId,
+		})
+	})
+
+	r.GET("/products", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"data": []string{"Product 1", "Product 2", "Product 3"},
+		})
+	})
+
+	r.GET("/product/:product_name", func(ctx *gin.Context) {
+		productName := ctx.Param("product_name")
+
+		price := ctx.Query("price")
+		color := ctx.Query("color")
+		ctx.JSON(200, gin.H{
+			"data": gin.H{
+				"product_name": productName,
+				"price":        price,
+				"color":        color,
+			},
 		})
 	})
 
