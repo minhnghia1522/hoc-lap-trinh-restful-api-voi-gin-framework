fd3512d6690c3eebf144ed836aae62feaaa474ab
minhnghia1522 <lmnghia22@gmail.com>
2026-06-19 10:23:01 +0700

    Enhance validation error handling and add new validation rules for email, datetime, and file extensions

--- lesson04-route-validation/utils/validation.go
+++ lesson04-route-validation/utils/validation.go
@@ -2,8 +2,9 @@ package utils
 
 import (
 	"fmt"
-	"log"
+	"path/filepath"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -23,34 +24,43 @@ func HandleValidationErrors(err error) gin.H {
 		errorMap := make(map[string]string)
 
 		for _, e := range validationError {
-			log.Printf("%+v", e.Tag())
-			log.Printf("%+v", e.Field())
-
+			fieldPath := e.Field()
+			param := e.Param()
 			switch e.Tag() {
 			case "required":
-				errorMap[e.Field()] = e.Field() + " is required"
+				errorMap[fieldPath] = fieldPath + " is required"
 			case "gt":
-				errorMap[e.Field()] = fmt.Sprintf("%s must be greater than %s", e.Field(), e.Param())
+				errorMap[fieldPath] = fmt.Sprintf("%s must be greater than %s", fieldPath, param)
 			case "lt":
-				errorMap[e.Field()] = fmt.Sprintf("%s must be less than %s", e.Field(), e.Param())
+				errorMap[fieldPath] = fmt.Sprintf("%s must be less than %s", fieldPath, param)
 			case "gte":
-				errorMap[e.Field()] = fmt.Sprintf("%s must be greater than or equal to %s", e.Field(), e.Param())
+				errorMap[fieldPath] = fmt.Sprintf("%s must be greater than or equal to %s", fieldPath, param)
 			case "lte":
-				errorMap[e.Field()] = fmt.Sprintf("%s must be less than or equal to %s", e.Field(), e.Param())
+				errorMap[fieldPath] = fmt.Sprintf("%s must be less than or equal to %s", fieldPath, param)
 			case "uuid":
-				errorMap[e.Field()] = e.Field() + " must be a valid UUID"
+				errorMap[fieldPath] = fieldPath + " must be a valid UUID"
 			case "slug":
-				errorMap[e.Field()] = e.Field() + " must contain only lowercase letter, numbers, hyphens and dots."
+				errorMap[fieldPath] = fieldPath + " must contain only lowercase letter, numbers, hyphens and dots."
 			case "min":
-				errorMap[e.Field()] = fmt.Sprintf("%s must be greater than %s characters.", e.Field(), e.Param())
+				errorMap[fieldPath] = fmt.Sprintf("%s must be greater than %s characters.", fieldPath, param)
 			case "max":
-				errorMap[e.Field()] = fmt.Sprintf("%s must be less than %s characters.", e.Field(), e.Param())
+				errorMap[fieldPath] = fmt.Sprintf("%s must be less than %s characters.", fieldPath, param)
+			case "min_int":
+				errorMap[fieldPath] = fmt.Sprintf("%s must have a greater value than %s", fieldPath, param)
+			case "max_int":
+				errorMap[fieldPath] = fmt.Sprintf("%s must have a value less than %s", fieldPath, param)
 			case "oneof":
-				allowedValues := strings.Join(strings.Split(e.Param(), " "), ", ")
-				errorMap[e.Field()] = fmt.Sprintf("%s must be one of %s.", e.Field(), allowedValues)
+				allowedValues := strings.Join(strings.Split(param, " "), ", ")
+				errorMap[fieldPath] = fmt.Sprintf("%s must be one of %s.", fieldPath, allowedValues)
 			case "search":
-				errorMap[e.Field()] = e.Field() + " must contain only lowercase letter, numbers, hyphens and dots."
-
+				errorMap[fieldPath] = fieldPath + " must contain only lowercase letter, numbers, hyphens and dots."
+			case "email":
+				errorMap[fieldPath] = fmt.Sprintf("%s must be in the correct email format", fieldPath)
+			case "datetime":
+				errorMap[fieldPath] = fmt.Sprintf("%s must follow the YYYY-MM-DD format exactly", fieldPath)
+			case "file_ext":
+				allowedValues := strings.Join(strings.Split(param, " "), ",")
+				errorMap[fieldPath] = fmt.Sprintf("%s chỉ cho phép những file có extension: %s", fieldPath, allowedValues)
 			}
 		}
 		return gin.H{"error": errorMap}
@@ -75,6 +85,46 @@ func RegisterValidators() error {
 	var searchRegex = regexp.MustCompile(`^[a-zA-Z0-9\s]+$`)
 	v.RegisterValidation("search", func(fl validator.FieldLevel) bool {
 		return searchRegex.MatchString(fl.Field().String())
+	})
+
+	v.RegisterValidation("min_int", func(fl validator.FieldLevel) bool {
+		minStr := fl.Param()
+		minVal, err := strconv.ParseInt(minStr, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		return fl.Field().Int() >= minVal
+	})
+
+	v.RegisterValidation("max_int", func(fl validator.FieldLevel) bool {
+		maxStr := fl.Param()
+		maxVal, err := strconv.ParseInt(maxStr, 10, 64)
+		if err != nil {
+			return false
+		}
+
+		return fl.Field().Int() <= maxVal
+	})
+
+	v.RegisterValidation("file_ext", func(fl validator.FieldLevel) bool {
+		filename := fl.Field().String()
+
+		allowedStr := fl.Param()
+		if allowedStr == "" {
+			return false
+		}
+
+		allowedExt := strings.Fields(allowedStr)
+		ext := strings.TrimPrefix(strings.ToLower(filepath.Ext(filename)), ".")
+
+		for _, allowed := range allowedExt {
+			if ext == strings.ToLower(allowed) {
+				return true
+			}
+		}
+
+		return false
 	})
 
 	return nil
