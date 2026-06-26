e50bfd427168bc34f293ee6aa2467879af4c99d0
minhnghia1522 <lmnghia22@gmail.com>
2026-06-26 16:05:08 +0700

    Gin #58: Tối Ưu Logging & Recovery - Log JSON Đẹp, Stack Trace Gọn! Bắt bug nhanh

--- lesson12-project-shopping-cart/internal/routes/routes.go
+++ lesson12-project-shopping-cart/internal/routes/routes.go
@@ -2,9 +2,10 @@ package routes
 
 import (
 	"user-management-api/internal/middleware"
+	"user-management-api/internal/utils"
+	"user-management-api/pkg/logger"
 
 	"github.com/gin-gonic/gin"
-	"github.com/natefinch/lumberjack"
 	"github.com/rs/zerolog"
 )
 
@@ -13,33 +14,33 @@ type Route interface {
 }
 
 func RegisterRoutes(r *gin.Engine, routes ...Route) {
-	httpLogger := zerolog.New(&lumberjack.Logger{
-		Filename:   "../../internal/logs/http.log",
-		MaxSize:    1, // megabytes
-		MaxBackups: 5,
-		MaxAge:     5,    //days
-		Compress:   true, // disabled by default
-		LocalTime:  true,
-	}).With().Timestamp().Logger()
+	httpLogger := newLoggerWithPath("../../internal/logs/recovery.log", "info")
+	recoveryLogger := newLoggerWithPath("../../internal/logs/recovery.log", "warning")
 
-	recoveryLogger := zerolog.New(&lumberjack.Logger{
-		Filename:   "../../internal/logs/recovery.log",
-		MaxSize:    1, // megabytes
-		MaxBackups: 5,
-		MaxAge:     5,    //days
-		Compress:   true, // disabled by default
-		LocalTime:  true,
-	}).With().Timestamp().Logger()
-
-	r.Use(middleware.LoggerMiddleware(&httpLogger),
-		middleware.RecoveryMiddleware(&recoveryLogger),
+	r.Use(
+		middleware.RateLimiterMiddleware(),
+		middleware.LoggerMiddleware(httpLogger),
+		middleware.RecoveryMiddleware(recoveryLogger),
 		middleware.APIKeyMiddleware(),
 		middleware.AuthMiddleware(),
-		middleware.RateLimiterMiddleware())
+	)
 
 	api := r.Group("/api/v1")
 
 	for _, route := range routes {
 		route.Register(api)
 	}
+}
+
+func newLoggerWithPath(path string, level string) *zerolog.Logger {
+	config := logger.LoggerConfig{
+		Level:       level,
+		FileName:    path,
+		MaxSize:     1, // megabytes
+		MaxBackups:  5,
+		MaxAge:      5,    //days
+		Compress:    true, // disabled by default
+		Environment: utils.GetEnv("APP_ENV", "development"),
+	}
+	return logger.NewLogger(config)
 }
---
+++ lesson12-project-shopping-cart/pkg/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"io"
+	"os"
+	"time"
+
+	"github.com/natefinch/lumberjack"
+	"github.com/rs/zerolog"
+)
+
+type LoggerConfig struct {
+	Level       string
+	FileName    string
+	MaxSize     int
+	MaxBackups  int
+	MaxAge      int
+	Compress    bool
+	Environment string
+}
+
+func NewLogger(config LoggerConfig) *zerolog.Logger {
+	zerolog.TimeFieldFormat = time.RFC3339
+	lvl, err := zerolog.ParseLevel(config.Level)
+	if err != nil {
+		lvl = zerolog.InfoLevel
+	}
+	zerolog.SetGlobalLevel(lvl)
+
+	var writer io.Writer
+	if config.Environment == "development" {
+		writer = os.Stdout
+	} else {
+		writer = &lumberjack.Logger{
+			Filename:   config.FileName,
+			MaxSize:    config.MaxSize, // megabytes
+			MaxBackups: config.MaxBackups,
+			MaxAge:     config.MaxAge,   //days
+			Compress:   config.Compress, // disabled by default
+			LocalTime:  true,
+		}
+	}
+	logger := zerolog.New(writer).With().Timestamp().Logger()
+
+	return &logger
+}
